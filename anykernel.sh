520064a4a4d991291ce53f9aa8660eb667f699ae
anarkia1976 <stefano.villa1976@gmail.com>
2015-01-06 14:50:17 +0100

    anykernel2: interactive and cpu-boost tuning II

--- anykernel.sh
+++ anykernel.sh
@@ -163,17 +163,17 @@ replace_string default.prop "ro.secure=0" "ro.secure=1" "ro.secure=0";
 # kernel tunables
 backup_file init.qcom-common.rc
 replace_line init.qcom-common.rc "write /sys/block/mmcblk0/queue/scheduler row" "    write /sys/block/mmcblk0/queue/scheduler deadline";
-replace_line init.qcom-common.rc "write /sys/block/mmcblk0/bdi/read_ahead_kb 512" "    write /sys/block/mmcblk0/bdi/read_ahead_kb 256";
+#replace_line init.qcom-common.rc "write /sys/block/mmcblk0/bdi/read_ahead_kb 512" "    write /sys/block/mmcblk0/bdi/read_ahead_kb 256";
 
 # interactive tunables
-replace_line init.qcom-common.rc "write /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load 90" "    write /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load 80";
+replace_line init.qcom-common.rc "write /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load 90" "    write /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load 70";
 replace_line init.qcom-common.rc "write /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq 1190400" "    write /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq 1728000";
 
 # cpu-boost tunables
-replace_line init.qcom-common.rc "write /sys/module/cpu_boost/parameters/boost_ms 20" "    write /sys/module/cpu_boost/parameters/boost_ms 40";
+replace_line init.qcom-common.rc "write /sys/module/cpu_boost/parameters/boost_ms 20" "    write /sys/module/cpu_boost/parameters/boost_ms 60";
 replace_line init.qcom-common.rc "write /sys/module/cpu_boost/parameters/sync_threshold 1728000" "    write /sys/module/cpu_boost/parameters/sync_threshold 1958400";
 replace_line init.qcom-common.rc "write /sys/module/cpu_boost/parameters/input_boost_freq 1497600" "    write /sys/module/cpu_boost/parameters/input_boost_freq 1728000";
-replace_line init.qcom-common.rc "write /sys/module/cpu_boost/parameters/input_boost_ms 40" "    write /sys/module/cpu_boost/parameters/input_boost_ms 80";
+replace_line init.qcom-common.rc "write /sys/module/cpu_boost/parameters/input_boost_ms 40" "    write /sys/module/cpu_boost/parameters/input_boost_ms 100";
 
 # panel and gamma
 replace_line init.qcom-common.rc "chown system graphics /sys/devices/virtual/graphics/fb0/panel_calibration" "    chown system system /sys/devices/virtual/graphics/fb0/panel_calibration";
