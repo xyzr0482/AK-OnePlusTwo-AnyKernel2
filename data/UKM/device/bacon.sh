8cf61ccaade65fba3f3d2480e9940b12ac6ec887
anarkia1976 <stefano.villa1976@gmail.com>
2015-06-16 11:53:42 +0200

    ukm: Version 3.8.3.3_Special_Ed

--- data/UKM/device/bacon.sh
+++ data/UKM/device/bacon.sh
@@ -388,7 +388,7 @@ case "$1" in
 			$BB echo "`$BB uname -r`"
 	;;
 		LiveInfoCurrent)
-			$BB echo "Version: 3.8.3.2 Special Edition"
+			$BB echo "Version: 3.8.3.3 Special Edition"
 	;;
 		LiveBrickedHotplug)
 			$BB echo "Bricked Hotplug Driver"
